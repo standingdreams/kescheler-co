879316fa6a529bedad9a06171e873c2df3cc1c3e
Douglas Rogers <designer.drogers@gmail.com>
2020-07-14 10:33:04 -0400

    Added new blogroll page
    Created breadcrumbs

---
+++ src/components/breadcrumbs.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const breadcrumbs = () => {
+  return (
+    <section className="c-breadcrumbs">
+      <div className="l-container">
+        <nav>
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Blog</a></li>
+            <li><a href="/">Without Sidebar</a></li>
+          </ul>
+        </nav>
+      </div>
+    </section>
+  )
+}
+
+export default breadcrumbs
---
+++ src/pages/blogroll.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/layout"
+import Breadcrumbs from "../components/breadcrumbs"
+
+const Blogroll = () => {
+  return (
+    <Layout title="Blog Roll">
+      <Breadcrumbs />
+    </Layout>
+  )
+}
+
+export default Blogroll
