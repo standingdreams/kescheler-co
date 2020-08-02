0b5697eb49eae2d5187b5c76e8d7ed7c1828cda1
Douglas Rogers <designer.drogers@gmail.com>
2020-08-02 16:16:16 -0400

    Updated meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Kescheler & Co.`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@codeOvrCoffee`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
