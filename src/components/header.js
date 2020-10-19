50c257db3551ad904bd035725b78cf5edad66bcc
Douglas Rogers <designer.drogers@gmail.com>
2020-10-19 15:37:39 -0400

    Updated masthead/nav
    Mobile adjustments for logo and overall area
    Added blog link to nav

--- src/components/header.js
+++ src/components/header.js
@@ -12,6 +12,7 @@ const Header = props => (
       <nav className="c-mainNav">
         <ul>
           <li><Link to="/">Home</Link></li>
+          <li><Link to="/blog">Blog</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
       </nav>
