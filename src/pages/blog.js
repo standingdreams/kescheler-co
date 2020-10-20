981e4f18c687f2dc01c9635e8331fe28f35047c2
Douglas Rogers <designer.drogers@gmail.com>
2020-10-20 14:36:42 -0400

    ESLint changes

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import dayjs from 'dayjs'
+import React from "react"
+import { graphql } from "gatsby"
+import dayjs from "dayjs"
+import { PropTypes } from "prop-types"
 
 import Layout from "../components/layout"
 import Breadcrumbs from "../components/breadcrumbs"
-import Pagination from '../components/Pagination';
+import Pagination from "../components/Pagination"
 
 const Blog = ({ data, pageContext }) => {
   const postsPerPage = data.prismicGlobal.data.posts_per_page
@@ -17,42 +18,36 @@ const Blog = ({ data, pageContext }) => {
       <section className="c-blogroll">
         <div className="l-container c-blogroll__grid">
           {data.posts.edges.map(post => {
-            const imageBlock = post.node.content.body.filter(bodyContent => bodyContent.slice_type === "image_block");
+            const imageBlock = post.node.content.body.filter(bodyContent => bodyContent.slice_type === "image_block")
 
             return (
               <div className="c-blogroll__post" key={post.node.id}>
-                {imageBlock.length > 0 &&
+                {imageBlock.length > 0 && (
                   <figure className="c-blogroll__postImage">
-                    <img src={imageBlock[0].items[0].image.url} alt=""/>
+                    <img src={imageBlock[0].items[0].image.url} alt="" />
                   </figure>
-                }
+                )}
                 <div className="c-blogroll__date">
-                  {dayjs(post.node.first_publication_date).format('MMM')}
-                  <span>{dayjs(post.node.first_publication_date).format('DD')}</span>
+                  {dayjs(post.node.first_publication_date).format("MMM")}
+                  <span>{dayjs(post.node.first_publication_date).format("DD")}</span>
                 </div>
                 <h2 className="c-blogroll__heading el-h2">
                   <span className="el-small">
-                    {post.node.tags.map((tag, index) => index === 0 ? tag : ` | ${tag}`)}
+                    {post.node.tags.map((tag, index) => (index === 0 ? tag : ` | ${tag}`))}
                   </span>
-                  <a href={`/post/${post.node.uid}`}>
-                    {post.node.content.post_title[0].text}
-                  </a>
+                  <a href={`/post/${post.node.uid}`}>{post.node.content.post_title[0].text}</a>
                 </h2>
                 <div className="c-blogroll__postContent">
-                  <p>
-                    {post.node.content.excerpt[0].text}
-                  </p>
+                  <p>{post.node.content.excerpt[0].text}</p>
                   <div className="c-blogroll__meta">
-                    <div className="c-blogroll__author">
-                      By Kescheler Nix-Powell
-                    </div>
+                    <div className="c-blogroll__author">By Kescheler Nix-Powell</div>
                   </div>
                 </div>
               </div>
             )
           })}
         </div>
-        {totalPages > 1 &&
+        {totalPages > 1 && (
           <Pagination
             base="/blog"
             currentPage={pageContext.currentPage || 1}
@@ -60,10 +55,15 @@ const Blog = ({ data, pageContext }) => {
             skip={pageContext.skip}
             postsCount={postsCount}
           />
-        }
+        )}
       </section>
     </Layout>
   )
+}
+
+Blog.propTypes = {
+  data: PropTypes.object,
+  pageContext: PropTypes.object,
 }
 
 export default Blog
--- src/templates/BlogPosts.js
+++ src/templates/BlogPosts.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from "react"
 import { graphql } from "gatsby"
-import dayjs from 'dayjs'
-import { RichText } from 'prismic-reactjs';
+import dayjs from "dayjs"
+import { RichText } from "prismic-reactjs"
+import { PropTypes } from "prop-types"
 
 import Layout from "../components/layout"
 
@@ -10,12 +11,14 @@ export default function SinglePostPage({ data }) {
   const { post_title, body } = content
 
   return (
-    <Layout title={ post_title[0].text } bodyClass="home">
+    <Layout title={post_title[0].text} bodyClass="home">
       <section className="c-banner c-banner--home">
         <div className="l-container">
           <div className="c-banner__content">
-            <h1 className="el-h1 c-banner__heading">{ post_title[0].text }</h1>
-            <span className="c-banner__breadcrumbs">Home / Blog / <span className="c-banner__breadcrumbs--current">{ post_title[0].text }</span></span>
+            <h1 className="el-h1 c-banner__heading">{post_title[0].text}</h1>
+            <span className="c-banner__breadcrumbs">
+              Home / Blog / <span className="c-banner__breadcrumbs--current">{post_title[0].text}</span>
+            </span>
           </div>
         </div>
         <div className="c-banner__infoCard c-banner__infoCard--blogPost">
@@ -29,7 +32,7 @@ export default function SinglePostPage({ data }) {
           </div>
           <div className="c-banner__infoCardContent">
             <span className="c-banner__infoCardTag">Posted</span>
-            <p>{dayjs(first_publication_date).format('MM.DD.YYYY')}</p>
+            <p>{dayjs(first_publication_date).format("MM.DD.YYYY")}</p>
           </div>
         </div>
       </section>
@@ -38,11 +41,16 @@ export default function SinglePostPage({ data }) {
           switch (block.slice_type) {
             case "quote_block":
               return (
-                <section className="p-blogPost__block p-blogPost__block--fullMobile" key={`${block.slice_type}-${index}`}>
+                <section
+                  className="p-blogPost__block p-blogPost__block--fullMobile"
+                  key={`${block.slice_type}-${index}`}
+                >
                   <div className="l-container">
                     <div className="p-blogPost__quote">
                       <h2 className="p-blogPost__quoteHeading">{block.primary.quote}</h2>
-                      <span className="p-blogPost__quoteAuthor">{block.primary.author}, {block.primary.authors_title}</span>
+                      <span className="p-blogPost__quoteAuthor">
+                        {block.primary.author}, {block.primary.authors_title}
+                      </span>
                     </div>
                   </div>
                 </section>
@@ -56,30 +64,27 @@ export default function SinglePostPage({ data }) {
                       <div className="p-blogPost__imageWrap p-blogPost__imageWrap--multi">
                         {block.items.map((imageItem, index) => (
                           <figure className="p-blogPost__image" key={`image-${index}`}>
-                            <img src={imageItem.image.url} alt=""/>
+                            <img src={imageItem.image.url} alt="" />
                           </figure>
                         ))}
                       </div>
                     </div>
                   </section>
                 )
-              } else {
-                return (
-                  <section className="p-blogPost__block p-blogPost__block--image" key={`${block.slice_type}-${index}`}>
-                    <figure className="p-blogPost__image" style={{backgroundImage: `url(${block.items[0].image.url})`}}>
-                      <img src={block.items[0].image.url} alt=""/>
-                    </figure>
-                  </section>
-                )
               }
+              return (
+                <section className="p-blogPost__block p-blogPost__block--image" key={`${block.slice_type}-${index}`}>
+                  <figure className="p-blogPost__image" style={{ backgroundImage: `url(${block.items[0].image.url})` }}>
+                    <img src={block.items[0].image.url} alt="" />
+                  </figure>
+                </section>
+              )
 
             default:
               return (
                 <section className="p-blogPost__block" key={`${block.slice_type}-${index}`}>
                   <div className="l-container">
-                    <div className="p-blogPost__copy">
-                      {RichText.render(block.primary.content_block)}
-                    </div>
+                    <div className="p-blogPost__copy">{RichText.render(block.primary.content_block)}</div>
                   </div>
                 </section>
               )
@@ -90,9 +95,13 @@ export default function SinglePostPage({ data }) {
   )
 }
 
+SinglePostPage.propTypes = {
+  data: PropTypes.object,
+}
+
 export const query = graphql`
   query($id: String!) {
-    post: prismicBlogPosts(id: {eq: $id}) {
+    post: prismicBlogPosts(id: { eq: $id }) {
       first_publication_date
       type
       content: data {
